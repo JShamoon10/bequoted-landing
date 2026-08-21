567065a9208a13ed43e66e9abe40b4c1d632c6bd
JShamoon10 <joeshamoon@startavision.ca>
2026-08-21 16:28:28 -0400

    Fix Vite multi-page HTML build entries

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        privacy: 'privacy.html',
+        support: 'support.html'
+      }
+    }
   }
 });
